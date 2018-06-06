9f0ac6b5083aea9bcd5cffe81ec98c84a1e51ef0
Kilmar Cabrera <00137316@uca.edu.sv>
2018-06-06 11:34:30 -0600

    nada

---
+++ app/src/main/java/com/cabrera/parcial2_00137316/Log_In.java
@@ -0,0 +1,13 @@
+package com.cabrera.parcial2_00137316;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Log_In extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_log__in);
+    }
+}

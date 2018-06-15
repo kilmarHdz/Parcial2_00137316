ee8b5beb3c3c249ccc06e469419ba770a3964699
Kilmar Cabrera <00137316@uca.edu.sv>
2018-06-15 14:34:33 -0600

    fragments

---
+++ app/src/main/java/com/cabrera/parcial2_00137316/Fragment/Top.java
@@ -0,0 +1,31 @@
+package com.cabrera.parcial2_00137316.Fragment;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.cabrera.parcial2_00137316.R;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class Top extends Fragment {
+
+
+    public Top() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_top, container, false);
+    }
+
+}
--- app/src/main/java/com/cabrera/parcial2_00137316/Fragment/homeNews.java
+++ app/src/main/java/com/cabrera/parcial2_00137316/Fragment/homeNews.java
@@ -1,4 +1,4 @@
-package com.cabrera.parcial2_00137316;
+package com.cabrera.parcial2_00137316.Fragment;
 
 
 import android.os.Bundle;
@@ -6,6 +6,8 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.cabrera.parcial2_00137316.R;
 
 
 /**
